e62b84b4050603324dcf54fc5db970634dede21e
zar <zar@qq.com>
2022-05-16 23:11:51 +0800

    更新内容

---
+++ git_001_web/src/main/java/com/bjpoernode/Controller22.java
@@ -0,0 +1,7 @@
+package com.bjpoernode;
+
+public class Controller22 {
+    public static void main(String[] args) {
+        System.out.println("222222222222222");
+    }
+}
--- git_002_javase/src/main/java/com/bjpowernode/Demo.java
+++ git_002_javase/src/main/java/com/bjpowernode/Demo.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("++++++++Hello world +++++javaSE!++++++++");
+        System.out.println("++++++++Hello world +++++javaSE22222222222++++++++");
     }
 }
